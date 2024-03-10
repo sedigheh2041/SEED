3e1f2d0dea8552dab958929e4bee5e279a912f82
ahmadi <ahmadi@meshkan.com>
2024-03-10 15:25:39 +0330

    fix: maven config file

--- src/test/java/com/example/demo234/TestDemo234Application.java
+++ src/test/java/com/example/demo234/TestDemo234Application.java
@@ -2,47 +2,9 @@ package com.example.demo234;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MSSQLServerContainer;
-import org.testcontainers.containers.MariaDBContainer;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.oracle.OracleContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestDemo234Application {
-
-    @Bean
-    @ServiceConnection
-    MariaDBContainer<?> mariaDbContainer() {
-        return new MariaDBContainer<>(DockerImageName.parse("mariadb:latest"));
-    }
-
-    @Bean
-    @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
-    }
-
-    @Bean
-    @ServiceConnection
-    OracleContainer oracleFreeContainer() {
-        return new OracleContainer(DockerImageName.parse("gvenzl/oracle-free:latest"));
-    }
-
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-    }
-
-    @Bean
-    @ServiceConnection
-    MSSQLServerContainer<?> sqlServerContainer() {
-        return new MSSQLServerContainer<>(DockerImageName.parse("mcr.microsoft.com/mssql/server:latest"));
-    }
 
     public static void main(String[] args) {
         SpringApplication.from(Demo234Application::main).with(TestDemo234Application.class).run(args);
